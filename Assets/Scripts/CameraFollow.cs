c8e1b13cc28caf06f97cc3e1f5c00452f749bd3e
soundpulse <soundpulse0801@gmail.com>
2018-12-01 16:01:55 +0000

    Added camera follow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -13,13 +13,16 @@ public class CameraFollow : MonoBehaviour {
     [SerializeField]
     private float yMin;
 
+    private Transform target;
+
     // Use this for initialization
     void Start () {
-		
+        target = GameObject.Find("Player").transform;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void LateUpdate () {
+        transform.position = new Vector3(Mathf.Clamp(target.position.x, xMin,xMax), Mathf.Clamp(target.position.y, yMin,yMax), transform.position.z);
 	}
 }
+
