bc2f1a3bac9831b50ffbc8cd793f47d46b710bcc
soundpulse <soundpulse0801@gmail.com>
2018-12-01 22:53:34 +0000

    lever Script

---
+++ Assets/Scripts/LeverScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverScript : MonoBehaviour
+{
+
+    public Sprite[] current_texture;
+    bool flicked;
+
+    private void Start()
+    {
+        flicked = false;
+    }
+    
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            flicked = !flicked;
+
+            //if not flicked, once pressed E, flicks lever
+            if (flicked == true) {
+            gameObject.GetComponent<SpriteRenderer>().sprite = current_texture[1];
+                //add code here
+            }
+            //If flicked, once pressed E, flicks it back
+            else
+            {
+            gameObject.GetComponent<SpriteRenderer>().sprite = current_texture[0];
+                //add code here
+            }
+
+
+
+
+
+        }
+    }
+}
--- Assets/Scripts/MovableObject.cs
+++ Assets/Scripts/MovableObject.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RigidBodySleep : MonoBehaviour {
+public class MovableObject : MonoBehaviour {
 
     [SerializeField]
     public Rigidbody2D rBody;
