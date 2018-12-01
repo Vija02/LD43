87fec616c3ee7ff2bcaa52818c139c408ffbbb19
MengCheang <c.cheang@newcastle.ac.uk>
2018-12-01 17:57:27 +0000

    Respawn v2

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -10,13 +10,19 @@ public class Respawn : MonoBehaviour
     [SerializeField]
     private Vector3 respawnPoint;
 
+    GameObject block;
+
+    void Start()
+    {
+        block = GameObject.Find("face-block");
+    }
+
     void Update()
     {
         if (transform.position.y < minHeightAllowed)
         {
-
             transform.position = respawnPoint;
-
+            block.transform.position = new Vector3(1.75f, -1.523046f, 0);
         }
     }
 }
