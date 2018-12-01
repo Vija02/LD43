a8a083495c9170760653ef4d62be5ed40e8bdab7
soundpulse <soundpulse0801@gmail.com>
2018-12-01 17:24:48 +0000

    feat: key bind to move box
    bugs presist, box floats if realeses shift

---
+++ Assets/Scripts/RigidBodySleep.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodySleep : MonoBehaviour {
+
+    [SerializeField]
+    public Rigidbody2D rBody;
+
+
+
+	// Use this for initialization
+	void Start () {
+        rBody.mass = 0.8f;
+        rBody.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            rBody.isKinematic = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            rBody.isKinematic = true;
+            rBody.velocity = Vector3.zero;
+            rBody.angularVelocity = 0;
+        }
+    }
+
+}
