7bd9fe03f945d14574f2916e0dc3682daf482bf1
MengCheang <c.cheang@newcastle.ac.uk>
2018-12-01 17:30:51 +0000

    Added Fall&Respawn script

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+
+    [SerializeField]
+    private float minHeightAllowed;
+    [SerializeField]
+    private Vector3 respawnPoint;
+
+    void Update()
+    {
+        if (transform.position.y < minHeightAllowed)
+        {
+
+            transform.position = respawnPoint;
+
+        }
+    }
+}
