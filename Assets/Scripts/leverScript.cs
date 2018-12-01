6b3222d95e45d93b20fe12ff54f2276353709abc
soundpulse <soundpulse0801@gmail.com>
2018-12-01 23:00:49 +0000

    feat: switch
    added usable switch (no functions yet and can access globally)

---
+++ Assets/Scripts/leverScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leverScript : MonoBehaviour {
+
+    public Sprite[]  current_texture;
+    bool flicked;
+
+	// Use this for initialization
+	void Start () {
+        flicked = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            flicked = !flicked;
+            if (flicked)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = current_texture[1];
+            }
+            else
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = current_texture[0];
+            }
+                
+        }
+	}
+}
