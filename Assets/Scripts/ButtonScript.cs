a2c17d42f3620b660c16e182c9c61520062ee933
soundpulse <soundpulse0801@gmail.com>
2018-12-01 23:40:31 +0000

    feat: functional button
    Press E to use the button (lever ?)

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+
+    public Sprite[] sprite;
+    public float timer;
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = sprite[1];
+            StartCoroutine("Release");
+               
+        }
+        
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(timer);
+        gameObject.GetComponent<SpriteRenderer>().sprite = sprite[0];
+    }
+
+}
