3ae4fcc391023aad04ff455bc7988c451df16e06
VIja02 <salim_michaelmi@yahoo.com>
2018-12-02 00:21:43 +0000

    feat: dragable block

--- Assets/Scripts/RigidBodySleep.cs
+++ Assets/Scripts/RigidBodySleep.cs
@@ -7,17 +7,17 @@ public class RigidBodySleep : MonoBehaviour {
     [SerializeField]
     public Rigidbody2D rBody;
 
+    public bool canHold = false;
 
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         rBody.mass = 0.8f;
         rBody.isKinematic = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-            
+
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             rBody.isKinematic = false;
@@ -25,10 +25,33 @@ public class RigidBodySleep : MonoBehaviour {
 
         if (Input.GetKeyUp(KeyCode.LeftShift))
         {
-            rBody.isKinematic = true;
-            rBody.velocity = Vector3.zero;
-            rBody.angularVelocity = 0;
+            stopMoving();
         }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            canHold = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            canHold = false;
+            stopMoving();
+        }
+    }
+
+    private void stopMoving()
+    {
+        rBody.isKinematic = true;
+        rBody.velocity = Vector3.zero;
+        rBody.angularVelocity = 0;
+        rBody.Sleep();
     }
 
 }
