73bbcd4891d8c5268b025b04c5332faf1036899f
soundpulse <soundpulse0801@gmail.com>
2018-12-02 15:39:51 +0000

    feat: teleport Script
    Input: position and player

---
+++ Assets/Scripts/TeleportScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportScript : MonoBehaviour {
+
+    public Transform TeleportTarget;
+    public GameObject Player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player.transform.position = TeleportTarget.transform.position;
+    }
+}
