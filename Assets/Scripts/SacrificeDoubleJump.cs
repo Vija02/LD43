524dbb80d257270e3a7c850610b0ddef84f66d37
soundpulse <soundpulse0801@gmail.com>
2018-12-02 01:43:24 +0000

    feat: button to disable doubleJump

---
+++ Assets/Scripts/SacrificeDoubleJump.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SacrificeDoubleJump : MonoBehaviour {
+
+    [SerializeField]
+    public GameObject player;
+    public Movement movementScript;
+
+    public void Button_Onclick()
+    {
+        //On clicking the button
+        //set enableDoubleJump to false
+        movementScript.enableDoubleJump = false;
+    }
+
+}
