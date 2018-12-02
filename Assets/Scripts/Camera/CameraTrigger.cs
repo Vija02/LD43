2e3822de45d9df8b76d63fee3fdc25085864d885
VIja02 <salim_michaelmi@yahoo.com>
2018-12-02 16:20:12 +0000

    plugin .-.

--- Assets/Scripts/Camera/CameraTrigger.cs
+++ Assets/Scripts/Camera/CameraTrigger.cs
@@ -4,19 +4,55 @@ using UnityEngine;
 public class CameraTrigger : MonoBehaviour
 {
 
+    private bool tped = false;
+    private int numOfPlayerInside = 0;
+
+    public GameObject tpPoint;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "Player")
         {
+            numOfPlayerInside++;
+
             Camera.main.GetComponent<CameraFollow>().enabled = false;
             Camera.main.GetComponent<CameraTarget>().target = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+
+            if (tped || !tpPoint)
+            {
+                return;
+            }
+
+            // TP the other player to the screen 
+            GameObject playerToTp;
+
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+            if (players[0].transform == collision.transform)
+            {
+                playerToTp = players[1];
+            }
+            else
+            {
+                playerToTp = players[0];
+            }
+
+            playerToTp.transform.position = tpPoint.transform.position;
+            tped = true;
         }
     }
     void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.transform.tag == "Player")
         {
+            numOfPlayerInside--;
+
             Camera.main.GetComponent<CameraFollow>().enabled = true;
+
+            if(numOfPlayerInside == 0)
+            {
+                tped = false;
+            }
+            
         }
     }
 }
