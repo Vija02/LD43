3ed956408fae7614e1e21183f6f44ce5f3e9a9d0
VIja02 <salim_michaelmi@yahoo.com>
2018-12-02 12:00:21 +0000

    again cause the plugin sucks

---
+++ Assets/Scripts/Reaper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reaper : MonoBehaviour {
+
+    public GameObject theReaper;
+
+    void Start()
+    {
+        theReaper.SetActive(false);   
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        theReaper.SetActive(true);
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        theReaper.SetActive(false);
+    }
+
+}
