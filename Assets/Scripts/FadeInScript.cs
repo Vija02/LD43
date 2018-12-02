33d6f95eb075816092dea83d793b785c29f44811
soundpulse <soundpulse0801@gmail.com>
2018-12-02 12:21:18 +0000

    feat: FadeIn and FadeOut Script
    animations for the grim reaper

---
+++ Assets/Scripts/FadeInScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInScript : MonoBehaviour {
+
+    SpriteRenderer rend;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<SpriteRenderer>();
+        Color c = rend.material.color;
+        c.a = 0f;
+        rend.material.color = c;
+        StartCoroutine("FadeIn");
+    }
+	
+
+    IEnumerator FadeIn()
+    {
+        for (float f=0.05f; f<=1; f += 0.05f)
+        {
+            Color c = rend.material.color;
+            c.a = f;
+            rend.material.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    public void StartFading()
+    {
+        //Add trigger here
+        //StartCoroutine("FadeIn");
+    }
+}
---
+++ Assets/Scripts/FadeOutScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOutScript : MonoBehaviour {
+
+    SpriteRenderer rend;
+
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        Color c = rend.material.color;
+        c.a = 0f;
+        rend.material.color = c;
+        StartCoroutine("FadeOut");
+    }
+
+
+    IEnumerator FadeOut()
+    {
+        for (float f = 1f; f >= -0.05f; f -= 0.05f)
+        {
+            Color c = rend.material.color;
+            c.a = f;
+            rend.material.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    public void StartFading()
+    {
+        //Add trigger here
+        //StartCoroutine("FadeOut");
+    }
+}
