34890a26b6f2e7c752050bbbfa80986edcc76cf0
VIja02 <salim_michaelmi@yahoo.com>
2018-12-03 01:50:16 +0000

    make reaper hover

---
+++ Assets/Scripts/Hover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour {
+
+    Vector2 floatY;
+    float originalY;
+
+    public float floatStrength;
+
+    void Start()
+    {
+        originalY = transform.position.y;
+    }
+
+    void Update()
+    {
+        floatY = transform.position;
+        floatY.y = (Mathf.Sin(Time.time) * floatStrength) + originalY;
+        transform.position = floatY;
+    }
+}
