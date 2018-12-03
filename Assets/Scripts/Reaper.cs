319e95f668a4253e996600109d493ae5c86eb060
VIja02 <salim_michaelmi@yahoo.com>
2018-12-03 02:10:42 +0000

    remove fade in, menu update

--- Assets/Scripts/Reaper.cs
+++ Assets/Scripts/Reaper.cs
@@ -3,41 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Reaper : MonoBehaviour
 {
-    public GameObject theReaper;
     public GameObject sacrificePanel;
-    public FadeInScript fadein;
-    public FadeOutScript fadeout;
 
-    private void Awake()
-    {
-        fadein = theReaper.GetComponent<FadeInScript>();
-        fadeout = theReaper.GetComponent<FadeOutScript>();
-    }
-
-    void Start()
-    {
-        //Disable Reaper
-        theReaper.SetActive(false);
-    }
-
-    //Reaper Fades in, Sacrifice panel activated
+    // Sacrifice panel activated
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.transform.tag == "Player")
         {
-            theReaper.SetActive(true);
             sacrificePanel.SetActive(true);
-            fadein.StartCoroutine("FadeIn");
         }
     }
 
-      //Reaper Fades out
     void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.transform.tag == "Player")
         {
-            fadeout.StartCoroutine("FadeOut");
+            sacrificePanel.SetActive(false);
         }
-        
     }
+
 }
