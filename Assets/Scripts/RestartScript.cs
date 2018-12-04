f766db34140b0e91b3616cf4ca63b7237387106b
soundpulse <soundpulse0801@gmail.com>
2018-12-04 00:33:19 +0000

    feat: restart
    not sure if it works

---
+++ Assets/Scripts/RestartScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScript : MonoBehaviour {
+    
+    [SerializeField]
+    private Scene scene;
+
+    private void Start()
+    {
+        scene = SceneManager.GetSceneAt(0);
+    }
+
+    public void Button_OnClick()
+    {
+        SceneManager.LoadScene(scene.name);
+    }
+}
