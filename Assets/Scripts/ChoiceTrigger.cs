eac9ada12831233a6e2c1df92f81b61be764a103
VIja02 <salim_michaelmi@yahoo.com>
2018-12-04 01:17:30 +0000

    last show menu

---
+++ Assets/Scripts/ChoiceTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoiceTrigger : MonoBehaviour
+{
+
+    public GameObject panel;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            panel.SetActive(true);
+        }
+    }
+}
