57cba40ffd3608bc3ed7bcd22b69580bf10f1a18
soundpulse <soundpulse0801@gmail.com>
2018-12-04 01:28:15 +0000

    feat: last bits

---
+++ Assets/Scripts/EndScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScript : MonoBehaviour {
+
+    [SerializeField]
+    private Scene scene;
+
+    public void Button_OnClick()
+    {
+        SceneManager.LoadScene("End");
+    }
+}
